3ec38be7c4564f59da955dc4cf7a7b5962eebdde
KautTanya <kaut.tanya.s@gmail.com>
2022-09-19 00:23:35 +0300

    task10 start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.getElementById('search-box');
+const listEl = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
